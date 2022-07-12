6421e126d34b2afe294b628c36e8e75061457d2f
WPAbhi <abhiashinsa770@gmail.com>
2022-07-12 11:26:53 +0530

    Student add, update, delete operations added

--- IJSE-Student Intake/src/controller/ManageStudentFormController.java
+++ IJSE-Student Intake/src/controller/ManageStudentFormController.java
@@ -33,22 +33,42 @@ public class ManageStudentFormController implements Initializable{
     public JFXButton btnList;
 
     public void addBtnOnAction(ActionEvent actionEvent) {
-//        Student student = new Student(txtId.getText(), txtName.getText(), txtMail.getText(),txtContact.getText(), txtAddress.getText(),txtNic.getText());
-//
-//        try {
-//            if (CrudUtil.execute("INSERT INTO student VALUES (?,?,?,?,?,?)",student.getId(), student.getName(), student.getMail(), student.getContact(), student.getAddress(), student.getNic())){
-//                new Alert(Alert.AlertType.CONFIRMATION, "Student Saved Successfully...!");
-//            }
-//        } catch (ClassNotFoundException | SQLException  e) {
-//            e.printStackTrace();
-//            new Alert(Alert.AlertType.ERROR, "Try Again....!");
-//        }
+        Student student = new Student(txtId.getText(), txtName.getText(), txtMail.getText(),txtContact.getText(), txtAddress.getText(),txtNic.getText());
+
+        try {
+            if (CrudUtil.execute("INSERT INTO student VALUES (?,?,?,?,?,?)",student.getId(), student.getName(), student.getMail(), student.getContact(), student.getAddress(), student.getNic())){
+                new Alert(Alert.AlertType.CONFIRMATION, "Student Saved Successfully...!").show();
+            }
+        } catch (ClassNotFoundException | SQLException  e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Try Again....!").show();
+        }
     }
 
     public void updateBtnOnAction(ActionEvent actionEvent) {
+        Student s=new Student(txtId.getText(), txtName.getText(), txtMail.getText(),txtContact.getText(), txtAddress.getText(),txtNic.getText());
+
+        try{
+            if(CrudUtil.execute("UPDATE student SET student_Name=?,email=?,contact=?, address=?, nic=?  WHERE student_Id=?",s.getName(), s.getMail(), s.getContact(), s.getAddress(), s.getNic(), s.getId())){
+                new Alert(Alert.AlertType.CONFIRMATION, "Student Updated Successfully...!").show();
+            }
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Try Again....!").show();
+        }
     }
 
     public void deleteBtnOnAction(ActionEvent actionEvent) {
+        try{
+            if (CrudUtil.execute("DELETE FROM student WHERE student_Id=?",txtId.getText())){
+                new Alert(Alert.AlertType.CONFIRMATION, "Student Deleted Successfully...!").show();
+            }else{
+                new Alert(Alert.AlertType.ERROR, "Try Again....!").show();
+            }
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Try Again....!"+e).show();
+        }
     }
 
     public void listBtnOnAction(ActionEvent actionEvent) {
@@ -56,49 +76,49 @@ public class ManageStudentFormController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-       // setStudentIds();
+       setStudentIds();
     }
 
     private void setStudentIds() {
-//        try {
-//
-//            ObservableList<String> ObList = FXCollections.observableArrayList(
-//                    StudentCrudController.getAllIds()
-//            );
-//            cmbStudentID.setItems(ObList);
-//
-//
-//            cmbStudentID.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-//                String id= (String) newValue;
-//
-//                ResultSet result = null;
-//                try {
-//                    result = CrudUtil.execute("SELECT * FROM student WHERE student_Id=?",id);
-//                } catch (SQLException throwables) {
-//                    throwables.printStackTrace();
-//                } catch (ClassNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//                try {
-//                    if (result.next()) {
-//                        txtId.setText(result.getString(1));
-//                        txtName.setText(result.getString(2));
-//                        txtMail.setText(result.getString(3));
-//                        txtContact.setText(result.getString(4));
-//                        txtAddress.setText(result.getString(5));
-//                        txtNic.setText(result.getString(6));
-//                    } else {
-//                        new Alert(Alert.AlertType.WARNING, "Empty Result").show();
-//                    }
-//                } catch (SQLException throwables) {
-//                    throwables.printStackTrace();
-//                }
-//
-//            });
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
+        try {
+
+            ObservableList<String> ObList = FXCollections.observableArrayList(
+                    StudentCrudController.getAllIds()
+            );
+            cmbStudentID.setItems(ObList);
+
+
+            cmbStudentID.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                String id= (String) newValue;
+
+                ResultSet result = null;
+                try {
+                    result = CrudUtil.execute("SELECT * FROM student WHERE student_Id=?",id);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (result.next()) {
+                        txtId.setText(result.getString(1));
+                        txtName.setText(result.getString(2));
+                        txtMail.setText(result.getString(3));
+                        txtContact.setText(result.getString(4));
+                        txtAddress.setText(result.getString(5));
+                        txtNic.setText(result.getString(6));
+                    } else {
+                        new Alert(Alert.AlertType.WARNING, "Empty Result").show();
+                    }
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+
+            });
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
