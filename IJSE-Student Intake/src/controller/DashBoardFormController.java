70fa24f8d5c1af9062dbe2f46dab522d00f51b41
WPAbhi <abhiashinsa770@gmail.com>
2022-07-12 10:27:06 +0530

    Ui Created

--- IJSE-Student Intake/src/controller/DashBoardFormController.java
+++ IJSE-Student Intake/src/controller/DashBoardFormController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+    public JFXButton btnStart;
+    public AnchorPane context;
+
+    public void startBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) btnStart.getScene().getWindow();
+        stage.close();
+
+        Parent parent = FXMLLoader.load(getClass().getResource("../view/ManageStudentForm.fxml"));
+        Stage stage2 = new Stage();
+        stage2.setScene(new Scene(parent));
+        stage2.setResizable(false);
+        stage2.show();
+    }
 }
---
+++ IJSE-Student Intake/src/controller/ManageStudentFormController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ManageStudentFormController {
+    public AnchorPane context;
+    public JFXComboBox cmbStudentID;
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtMail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+    public JFXButton btnAdd;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public JFXButton btnList;
+
+    public void addBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void listBtnOnAction(ActionEvent actionEvent) {
+    }
+}
